6c8a8cd654506da75fd582053472358a7db6654e
simhaRichard <owner@STUDENT-9>
2020-03-19 23:26:33 +0200

    add ray impl

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,4 +1,29 @@
 package primitives;
 
 public class Ray {
+    private Point3D point;
+    private Vector direction;
+
+    /**
+     * constructor
+     * @param newPoint
+     * @param unnormlizedVector
+     */
+    public Ray (Point3D newPoint, Vector unnormlizedVector) {
+        point = new Point3D(newPoint);
+        direction = new Vector(unnormlizedVector.normalized());
+    }
+
+    /**
+     * point getter
+     * @return point
+     */
+    public Point3D GetPoint(){ return point;}
+
+    /**
+     * direction getter
+     * @return direction
+     */
+    public Vector GetDirection() { return direction;}
+
 }
