3300f4ace54efcb9ba855752e61a896c4b4e4389
YHMatof <yosefma10@gmail.com>
2020-05-03 13:29:16 +0300

    color change

--- src/unittests/RenderTests.java
+++ src/unittests/RenderTests.java
@@ -25,8 +25,8 @@ public class RenderTests {
         Scene scene = new Scene("Test scene");
         scene.setCamera(new Camera(Point3D.ZERO, new Vector(0, 0, 1), new Vector(0, -1, 0)));
         scene.setDistance(100);
-        scene.setBackground(new Color(75, 127, 90));
-        scene.setAmbientLight(new AmbientLight(new Color(255, 191, 191), 1));
+        scene.setBackground(new Color(java.awt.Color.BLUE));
+        scene.setAmbientLight(new AmbientLight(new Color(255,255, 255),1));
 
         scene.addGeometries(new Sphere(50, new Point3D(0, 0, 100)));
 
@@ -40,7 +40,7 @@ public class RenderTests {
         Render render = new Render(imageWriter, scene);
 
         render.renderImage();
-        render.printGrid(50, java.awt.Color.YELLOW);
+        render.printGrid(50, java.awt.Color.BLACK);
         render.writeToImage();
     }
 }
